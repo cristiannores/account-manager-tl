daa97b19c236e14f7d0596083dad8170ee07d208
cristiannores <cristian.nores@gmail.com>
2021-01-25 05:27:31 -0300

    add some test & refactor

--- dist/adapters/controllers/example/container/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAllMembers = void 0;
-var getAllMembers_1 = require("../../../../application/modules/team/useCases/getAllMembers");
-var getAllMembers = new getAllMembers_1.UseCaseGetAllMembers();
-exports.getAllMembers = getAllMembers;
-//# sourceMappingURL=index.js.map
